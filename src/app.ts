ab98188a3b254dfe9b11605cfbfec5e006387bcd
wendy <chinwe.promise2016@gmail.com>
2023-09-15 19:44:16 +0100

    aded entry route

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/person", PersonRouter);
+app.get("/", (req: Request, res: Response) => {
+ res.status(200).json("Success!");
+});
 app.use("*", (req: Request, res: Response, next: NextFunction) => {
  const err: AppError = new AppError(`${req.originalUrl} not found!`, 404);
  next(err);
