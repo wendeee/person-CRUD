63c0b0e395a1c8b08b8a1a0e399e930ed0cf2ac2
wendy <chinwe.promise2016@gmail.com>
2024-02-03 10:06:54 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-const { Appsignal } = require("@appsignal/nodejs");
+// const { Appsignal } = require("@appsignal/nodejs");
 
-const appSignal = new Appsignal({
- active: true,
- name: "techbytes",
- // Your app's AppSignal Push API key. We don't recommend committing this key.
- // Set the Push API key using a system environment variable.
- // pushApiKey: process.env.APPSIGNAL_PUSH_API_KEY,
- pushApiKey: "adfe8e5e-00d4-413f-8e5d-762581a1e8cf",
-});
+// const appSignal = new Appsignal({
+//  active: true,
+//  name: "techbytes",
+//  // Your app's AppSignal Push API key. We don't recommend committing this key.
+//  // Set the Push API key using a system environment variable.
+//  // pushApiKey: process.env.APPSIGNAL_PUSH_API_KEY,
+//  pushApiKey: "adfe8e5e-00d4-413f-8e5d-762581a1e8cf",
+// });
 
 import { expressErrorHandler } from "@appsignal/nodejs";
 const { expressMiddleware } = require("@appsignal/express");
@@ -17,7 +17,7 @@ import { router as PersonRouter } from "./api/routes/person.route";
 const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(expressMiddleware(appSignal));
+// app.use(expressMiddleware());
 app.use("/api", PersonRouter);
 app.get("/", (req: Request, res: Response) => {
  res.status(200).json("Success!");
